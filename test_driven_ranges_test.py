0d5c9590dd93438218c04cbe64913b28968d379a
Sathyapriyan Kannan <sathyapriyankannan@gmail.com>
2022-03-18 09:47:29 +0530

    Add few more test cases

--- test_driven_ranges_test.py
+++ test_driven_ranges_test.py
@@ -10,6 +10,9 @@ class TestDrivenRangesTest(unittest.TestCase):
     def test_get_range(self):
         self.assertEqual(test_driven_ranges.get_continuous_ranges([3, 3, 5, 4, 10, 11, 12]), ['3-5, 4', '10-12, 3'])
         self.assertEqual(test_driven_ranges.get_continuous_ranges([]), 'INVALID_INPUTS')
+        self.assertEqual(test_driven_ranges.get_continuous_ranges([3]), [])
+        self.assertEqual(test_driven_ranges.get_continuous_ranges([3, 4]), ['3-4, 2'])
+
 
 if __name__ == '__main__':
     unittest.main()
