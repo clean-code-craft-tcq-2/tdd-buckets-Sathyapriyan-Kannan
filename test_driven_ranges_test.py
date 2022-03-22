3c442da604e2b86aac0b5cd7c56dc5d94ba4ee6d
Sathyapriyan Kannan <sathyapriyankannan@gmail.com>
2022-03-22 22:13:01 +0530

    Extension - TDD Step - 1

--- test_driven_ranges_test.py
+++ test_driven_ranges_test.py
@@ -14,6 +14,6 @@ class TestDrivenRangesTest(unittest.TestCase):
         self.assertEqual(test_driven_ranges.get_continuous_ranges([3, 4]), ['3-4, 2'])
 
     def test_get_most_frequent_reading(self):
-        self.assertEqual(test_driven_ranges.get_most_frequent_reading([3, 4, 5, 7, 7, 7, 7, 20,21,22], 7))
+        self.assertEqual(test_driven_ranges.get_most_frequent_reading([3, 4, 5, 7, 7, 7, 7, 20,21,22]), 7)
 
 unittest.main()
